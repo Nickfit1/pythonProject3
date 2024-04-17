dfc7041665d242f8d4966e568a8051bed2fd7b58
nikfit <nickfit@yandex.ru>
2024-04-17 21:12:06 +0300

    123

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,21 @@
 def add(x, y):
-    return x + y
+    return float(x) + float(y)
 
 
 def sub(x, y):
-    return x - y
+    return float(x) - float(y)
 
 
 def mul(x, y):
-    return x * y
+    return float(x) * float(y)
 
 
 def div(x, y):
-    return x / y
+    return float(x) / float(y)
 
 
 def sqrt(x):
-    return x**0.5
+    return float(x)**0.5
 
 
 # Main Program
@@ -26,28 +26,37 @@ print("3. Умножение")
 print("4. Деление")
 print("5. Квадратный корень")
 
-
+def check_input():
+    while True:
+        try:
+            num = input("Введите число: ")
+            if num.isdigit():
+                return num
+        except ValueError:
+            print('Это не число')
 while True:
     operation = input("Enter Choice (1/2/3/4/5): ")
 
     if operation in ('1', '2', '3', '4', '5'):
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
+        num1 = check_input()
+        num2 = check_input()
 
-        if operation == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+    if operation == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
 
-        elif operation == '2':
+    elif operation == '2':
             print(num1, "-", num2, "=", sub(num1, num2))
 
-        elif operation == '3':
+    elif operation == '3':
             print(num1, "*", num2, "=", mul(num1, num2))
 
-        elif operation == '4':
+    elif operation == '4':
+        try:
             print(num1, "/", num2, "=", div(num1, num2))
-
-        elif operation == '5':
-            print("Квадратный корень из", num1, "=", sqrt(num1))
-            print("Квадратный корень из", num2, "=", sqrt(num2))
+        except ZeroDivisionError:
+                print("Ошибка: На ноль делить нельзя!")
+    elif operation == '5':
+        print("Квадратный корень из", num1, "=", sqrt(num1))
+        print("Квадратный корень из", num2, "=", sqrt(num2))
 
 
--- test.py
+++ test.py
@@ -1,29 +1,11 @@
-from typing import Union, Optional
-
-EMPTY_CELL = " "
-def init_field(size: int, empty_cell: str = EMPTY_CELL) -> list[list]:
-    """
-    Создаёт и возвращет пустое поле для игры
-
-    :param size: размер поля
-    :param empty_cell: чем заполняется пустая ячейка
-    :return: отображение пустого поля в виде листа листов
-
-    """
-    # TODO Можно выбрать своё представление поля, допустим строковое или одномерный список
-    return [[empty_cell] * size for _ in range(size)]
-
-
-def draw_field(field):
-    """
-    Функция рисует поле
-    :param field: Объект поля
-    :return: None
-    """
-
-    field_ = init_field(3, '| |')
-    for a in field_:
-        print('_'*21)
-        print(a)
-    print('_' * 21)
-
+def check_input():
+    while True:
+        try:
+            num = input("Введите число: ")
+            if num.isdigit():
+                return num
+        except Exception:
+            print('Это не число')
+num1 = check_input()
+num2 = check_input()
+print(num2,num1)
